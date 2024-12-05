1c9d7e94f9e8a0ac98e525e6a9a7ab1ad4103985
dtoe07 <dpn.toe@gmail.com>
2024-12-04 19:25:59 -0800

    Version 2

--- alcoholic.py
+++ alcoholic.py
@@ -1,8 +1,8 @@
-# A bot scraper to search for specific liquor in Oregon when they are avail in store
-# Created by: Dinh dtoe07@gmail.com
-# Date created: 1/7/2021
+# Liquor Availability Bot Scraper for Oregon Liquor Stores
+# Purpose: Automates the search for specific liquor availability in Oregon liquor stores.
+# Author: Dinh Nguyen (dtoe07@gmail.com)
+# Date Created: January 7, 2021
 
-from urllib.request import urlopen
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -14,239 +14,212 @@ from random import randint
 import datetime
 
 
-# Function to send txt from Gmail to phone#
+def main():
+    # Browser User Agents to randomize browswer for the bot
+    user_agent_list = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    ]
+    user_agent = random.choice(user_agent_list)
+    session = requests.Session()
+    session.headers.update({'User-Agent': user_agent})
 
-def send_SMS(message, addr_list):
-    if message != 'None':
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login('YOUR_EMAIL_HERE', 'YOUR_PASSWORD_HERE')
-        from_mail = 'YOUR_EMAIL_HERE'
+    # Try to establish connection to www.oregonliquorsearch.com
+    try:
+        session.post("http://www.oregonliquorsearch.com/servlet/WelcomeController", timeout=10)
+    except requests.RequestException as e:
+        print(f"Failed to establish session: {e}")
+        return
 
-        # Recipients list to send emails to
+    # List of Recipients
+    addr_to = ['dpn.toe@gmail.com', 'haoqle@yahoo.com', '5038410916@tmomail.net', '9713345742@txt.att.net']
 
-        addr_to = addr_list
+    # List of the liquor to be searched
+    item_list = [
+        ('8722B', 'Red Weller'),
+        ('8954B', 'Green Weller'),
+        ('1562B', 'W.L. WELLER 12YR KENTUCKY STRAIGHT BRBN'),
 
-        # Setup the MIME
+        ('8119B', 'SUNTORY HIBIKI 12YR'),
+        ('7634B', 'SUNTORY YAMAZAKI 18 YR'),
 
-        mail_content = message
-        message = MIMEMultipart()
-        message['From'] = 'YOUR_EMAIL_HERE'
-        message['To'] = ', '.join(addr_to)
-        message['Subject'] = 'Cheers with Toe! '  # The subject line
+        ('0191B', 'Stagg JR.'),
+        ('2146B', "WOODFORD RES. MC BOURBON"),
+        ('3749B', "BLOOD OATH PACT VII"),
+        ('2893B', "HIGH WEST MIDWINTER"),
+        ('2657B ', "MICHTER'S TOASTED BARREL FINISH"),
 
-        # The body and the attachments for the mail
+        ('0793B ', "E.H. TAYLOR SINGLE BARREL STRAIGHT BOURB"),
+        ('1416B ', "E.H. TAYLOR JR BARREL PROOF"),
+        ('1418B ', "E.H. TAYLOR STRAIGHT RYE WHISKEY"),
 
-        message.attach(MIMEText(mail_content, 'plain'))
-        text = message.as_string()
+        ('5830B', 'TEQUILA FORTALEZA BLANCO'),
+        ('5831B', 'TEQUILA FORTALEZA REPOSADO'),
 
-        server.sendmail(from_mail, addr_to, text)
-
-        # Quit the server after job is done
-
-        server.quit()  # End send_message function!
-
-
-########################################################################################################################################
-
-# List of user agents for header of the requests
-
-user_agent_list = \
-    ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15'
-     ,
-     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
-     ,
-     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
-     ,
-     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0'
-     ,
-     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
-     ]
-
-# Pick a random user agent
-
-user_agent = random.choice(user_agent_list)
-
-# Open new session and click the button for over 21
-
-session = requests.Session()
-
-# Update user agent to header
-
-session.headers.update({'User-Agent': user_agent})
-
-# Start connecting to the server, this will auto bypass the click if you are over 21 button as well
-
-s = \
-    session.post('http://www.oregonliquorsearch.com/servlet/WelcomeController'
-                 )
-time.sleep(randint(10, 30))
-
-
-# Define a search function to take in an item number ======================================================================================
-
-def search_liquor(itemNumber, itemName):
-    NEWLINE = '\n'
-
-    # Getting result list from the search with the Item number
-
-    s = \
-        session.get('http://www.oregonliquorsearch.com/servlet/FrontController?view=global&action=search&radiusSearchParam=30&productSearchParam='
-                     + itemNumber
-                    + '&locationSearchParam=97230&chkDefault=on&btnSearch=Search'
-                    )
-
-    # Loading page content to bs4 for parsing
-
-    bsObj = BeautifulSoup(s.content, 'html.parser')
-
-    # find the section that contains the list of the stores that has it avail
-
-    result = bsObj.find('table', {'class': 'list'})
-
-    # Check if there is only one store avail to buy for that item
-
-    oneStore = bsObj.find('div', {'id': 'prod-loc-details'})
-
-    # Continue if stock is found!!!!
-
-    if result is not None and oneStore is None:
-        message = '****** '
-
-        # Append product name into the message to send SMS later
-
-        message += str(bsObj.find('th', {'id': 'product-desc'
-                       }).find('h2').text) + '\n'
-
-        # Print out the product name for debugging purposes
-
-        print '########## ' + bsObj.find('th', {'id': 'product-desc'
-                }).find('h2').text + ' ##########' + '\n'
-
-        # Loading up the stores to a list of stores
-
-        resultList = result.find_all('tr')
-
-        # Printing out each store info
-
-        for tag in resultList:
-
-            # Find store info
-
-            aStore = tag.find_all('td')
-
-            # Looping thru a specific range from the store info list to display
-
-            for item in aStore[2:6]:
-                print item.text
-
-            # Check if the list is not empty then print the quantity of the avail product
-
-            if len(aStore) > 7:
-                quantity = 'Quantity: ' + aStore[6].text + '\n'
-                print quantity
-                message += str(aStore[2].text + NEWLINE)
-                message += str(aStore[4].text + NEWLINE)
-                message += str(quantity + NEWLINE)
-
-        # append 2 extra spaces to the message because for some reason it will cut 2 pos when sending it out...
-
-        message += '  '
-        return message
-    elif oneStore is not None:
-
-    # If there is only one store has it
-
-        message = '****** '
-        message += str(bsObj.find('th', {'id': 'product-desc'
-                       }).find('h2').text) + '\n'  # append bottle name into the message to send SMS
-        print '########## ' + bsObj.find('th', {'id': 'product-desc'
-                }).find('h2').text + ' ##########' + '\n'
-        print 'only one store has it in stock: '
-        store = oneStore.find('td', {'id': 'location-display'})
-        storeInfo = store.find_all('p')
-        for info in storeInfo:
-            print info.text
-        print '\n'
-        address = str(storeInfo[0].text)
-        new_address = ' '.join(address.split())
-        message += new_address
-        message += str(NEWLINE + storeInfo[1].text + NEWLINE)
-
-        # Append 2 extra spaces again
-
-        message += '  \n'
-        return message
-    else:
-
-    # If there are none instock
-
-        print '########## none in stock for item: ' + itemName \
-            + ' ! ##########'
-        message = 'None'
-        return message
-
-
-###################################################################################################################
-################################ Start the search with each item codes ############################################
-###################################################################################################################
-
-addr_to = ['RECEIVER_EMAIL_HERE', 'TMOBILE_PHONE_HERE@tmomail.net']  # Recipients list to send SMS to
-
-# List of the products with their search codes
-
-item_list = [
-    ('8722B', 'Red Weller'),
-    ('8119B', 'SUNTORY HIBIKI 12YR'),
-    ('8321B', 'SUNTORY YAMAZAKI 12 YR'),
-    ('7634B', 'SUNTORY YAMAZAKI 18 YR'),
-    ('8954B', 'Green Weller'),
-    ('0191B', 'Stagg JR.'),
-    ('1562B', 'W.L. WELLER 12YR KENTUCKY STRAIGHT BRBN'),
-    ('2146B', 'WOODFORD RES. MC BOURBON'),
-    ('3749B', 'BLOOD OATH PACT VII'),
-    ('2893B', 'HIGH WEST MIDWINTER'),
-    ('2344B', "MICHTER'S US1 SOUR MASH WHISKEY"),
-    ('2657B ', "MICHTER'S TOASTED BARREL FINISH"),
-    ('0793B ', 'E.H. TAYLOR SINGLE BARREL STRAIGHT BOURB'),
-    ('1416B ', 'E.H. TAYLOR JR BARREL PROOF'),
-    ('1418B ', 'E.H. TAYLOR STRAIGHT RYE WHISKEY'),
-    ('6374B', 'WELLER FULL PROOF'),
+        ('2160B', 'EAGLE RARE S/B BOURBON')
+        # Add more items here...
     ]
 
-# randomize the list to make the search look more humanlike
+    # Randomize the liquor list
+    random.shuffle(item_list)
+    now = datetime.datetime.now()
 
-random.shuffle(item_list)
-now = datetime.datetime.now()
+    # Add to log message bot has started
+    log_message(f"Bot started at {now.strftime('%Y-%m-%d %H:%M:%S')}")
 
-# Write to logfile script has started
+    # Start email/text body by appending the current time
+    email_body = f"{now.strftime('%Y-%m-%d %H:%M:%S')}\n\n"
 
-with open('/home/python/logfile.txt', 'a') as myfile:
-    myfile.write('\n - Bot has started...   '
-                 + str(now.strftime('%Y-%m-%d %H:%M:%S')))
+    # Start searching thru the liquor list
+    for code, name in item_list:
+        message = search_liquor(session, code, name)    # Get message from searching each liquor
+        if message != 'None':
+            email_body += message                       # Appending the result if stock is found
+        time.sleep(randint(20, 40))                     # Randomly pause the bot after every search to simulate human-like behavior
 
-# Initialize a string to start loading up the email body to send later
+    # Sending email/text with all the results
+    send_SMS(email_body, addr_to)
+    # Add to the log file that the bot ran successfully
+    log_message("Bot completed successfully.")
 
-email_body = str(now.strftime('%Y-%m-%d %H:%M:%S')) + '''
 
- '''
+# Search for liquor function ===========================================================================================
+def search_liquor(session, item_number, item_name):
+    try:
+        # Attempt to send a GET request to the Oregon Liquor Search website
+        response = session.get(
+            f"http://www.oregonliquorsearch.com/servlet/FrontController?"
+            f"view=global&action=search&radiusSearchParam=30&productSearchParam={item_number}&"
+            f"locationSearchParam=97230&chkDefault=on&btnSearch=Search",
+            timeout=10  # Set a timeout for the request to avoid long delays
+        )
+        response.raise_for_status() # Raise an exception if the HTTP response indicates an error
+    except requests.RequestException as e:
+        # Handle any errors that occur during the request
+        print(f"Failed to fetch item {item_name}: {e}")
+        return 'None'   # Return 'None' to indicate the request failed.
 
-# Start searching from the list of products after ramdomized
+    # Parse the HTML content of the response
+    soup = BeautifulSoup(response.content, "html.parser")
 
-for (a, b) in item_list:
+    # Look for a table with class "list" that contains multiple store results
+    result = soup.find("table", {"class": "list"})
 
-    # Append the search result to the email message
+    # Look for a div with id "prod-loc-details" that contains details for a single store
+    one_store = soup.find("div", {"id": "prod-loc-details"})
 
-    message = search_liquor(a, b)
+    if result:
+        # If multiple store results are found, parse them using the `parse_multiple_stores` function
+        return parse_multiple_stores(soup, result)
+    elif one_store:
+        # If a single store result is found, parse it using the `parse_single_store` function
+        return parse_single_store(soup, one_store)
+    else:
+        # If no results are found, print a message indicating no stock was located
+        print(f"No stock found for item: {item_name}")
+        return 'None'   # Return 'None' to indicate no stock was found.
+
+
+# Parse details from multiple stores found in the search results =======================================================
+def parse_multiple_stores(soup, result):
+    # Start building the message with a separator line for clarity
+    message = '-------------------------\n'
+
+    # Extract the product description and format it.
+    message += " ".join(soup.find("th", {"id": "product-desc"}).find("h2").text.split()) + '\n'
+
+    # Loop through each table row in the search result.
+    for row in result.find_all("tr"):
+        store_info = row.find_all("td") # Extract table data cells for the current row
+        if len(store_info) > 7:
+            # If there are enough cells, extract and format store details
+            quantity = f"Quantity: {store_info[6].text}\n"  # Get the quantity of the product available
+            message += f"    - {store_info[2].text}\n"      # Add the store name
+            message += f"      {store_info[4].text}\n"      # Add the store location
+            message += f"      {quantity}\n"                # Add the quantity
+    return message      # Return the all results message
+
+
+# Parse details from a single store found in the search results ========================================================
+def parse_single_store(soup, one_store):
+    # Start building the message with a separator line for clarity
+    message = '-------------------------\n'
+
+    # Extract and format the product description
+    message += " ".join(soup.find("th", {"id": "product-desc"}).find("h2").text.split()) + '\n'
+
+    # Extract the store address and other information
+    store_info = one_store.find("td", {"id": "location-display"}).find_all("p")
+
+    if len(store_info) > 1:
+        # Extract and format the store address
+        address = str(store_info[0].text).strip()
+        new_address = " ".join(address.split())
+        message += f"    - {new_address}\n"
+
+        # Extract and add additional store details
+        store_details = str(store_info[1].text).strip()
+        message += f"      {store_details}\n"
+    else:
+        # If address or details are unavailable, log the issue in the message
+        message += "Store address or information not available\n"
+
+    # Add stock information if available.
+    in_stock_div = soup.find("div", {"id": "in-stock"})
+    if in_stock_div:
+        h2_tag = in_stock_div.find("h2")                # Extract the stock information from the stock section
+        if h2_tag:
+            stock_info = h2_tag.get_text(strip=True)    # Format the stock information
+            message += f"      {stock_info}\n"          # Add stock info to message
+        else:
+
+            message += "      Stock information not available\n"
+    else:
+        # If the stock section is not found, log this issue
+        message += "      Stock section not found\n"
+
+    return message + "  \n" # Add new line to the message.
+
+
+# Send the message via email to the specified recipients ===============================================================
+def send_SMS(message, recipients):
     if message != 'None':
-        email_body += message
-    time.sleep(randint(20, 40))
+        try:
+            server = smtplib.SMTP("smtp.gmail.com", 587)                # Connect to Gmail's SMTP server
+            server.starttls()                                           # Start TLS encryption for the connection
 
-# Sending out SMS and email with all the results
+            # Log in to the email account. Replace these with secure credentials
+            server.login('dtoe07@gmail.com', 'tkbbswgpmhvmhzng')
 
-send_SMS(email_body, addr_to)
+            # Create the email message
+            msg = MIMEMultipart()
+            msg['From'] = 'dtoe07@gmail.com'                            # Sender's email address
+            msg['To'] = ", ".join(recipients)                           # Recipient list
+            msg['Subject'] = 'dToe Liquor Availability Update'          # Email subject
+            msg.attach(MIMEText(message, 'plain'))                      # Attach the message as plain text
 
-# Write to logfile bot has run til the end
+            # Send the email
+            server.sendmail(msg['From'], recipients, msg.as_string())
+            server.quit()                                               # Close the connection to the SMTP server
+        except smtplib.SMTPException as e:
+            # Handle any SMTP exceptions and print an error message.
+            print(f"Failed to send email: {e}")
 
-with open('/home/python/logfile.txt', 'a') as myfile:
-    myfile.write(' ...Bot Succeeded!')
+
+# Append the given message to a log file ===============================================================================
+def log_message(message):
+    try:
+        with open("/home/python/logfile.txt", "a") as log_file:
+            log_file.write(f"{message}\n")                              # Write the message to the log file.
+    except IOError as e:
+        # Handle file input/output errors and print an error message
+        print(f"Logging error: {e}")
+
+
+if __name__ == "__main__":
+    main()  # Entry point for the script. Calls the main function (not defined here).
+            # Fuction can be used without having to define before the call
+            # This helps pushing all functions to the end of the file for cleaner setup
